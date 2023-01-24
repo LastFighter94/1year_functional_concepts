10d7d05136323ef6530cc584aab053ed75d19ed9
lastfighter94 <yurykishko@yandex.ru>
2023-01-24 17:20:59 +0300

    vue_3 continue

--- Js and Css/vue/vueproject/src/App.vue
+++ Js and Css/vue/vueproject/src/App.vue
@@ -1,40 +1,26 @@
 <template>
     <div class="app">
-        <form @submit.prevent>
-            <h4>Создание поста</h4>
-            <!-- @input=inputTitle -->
-            <input
-                v-bind:value="title" 
-                @input="title = $event.target.value"
-                class="input" 
-                type="text" 
-                placeholder="Название"
-            >
-            <input 
-                v-bind:value="body" 
-                @input="body = $event.target.value"
-                class="input" 
-                type="text" 
-                placeholder="Описание"
-            >
-            <button 
-                class="btn" 
-                @click="createPost"
-            >
-            Создать пост
-        </button>
-        </form>
-        <div 
-            class="post" 
-            v-for="post in posts"
-        >
-            <div><strong>{{ post.title }}</strong> {{ post.body }}</div>
-        </div>
+        <post-form
+            @create="createPost"
+        />
+        <post-list
+            :posts="posts"
+        />
+        <!-- <post-list v-bind:posts="posts"/> -->
     </div>
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue';
+import PostList from '@/components/PostList.vue';
+// import PostForm from './components/PostForm.vue';
+// import PostList from './components/PostList.vue';
+
 export default {
+    components: {
+        PostForm,
+        PostList
+    },
     data(){
         return {
             posts: [
@@ -42,25 +28,12 @@ export default {
                 {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
                 {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
                 {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-            ],
-            title: '',
-            body: ''
+            ]
         }
     },
     methods: {
-        createPost(){
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
-        },
-        inputTitle(event){
-            // console.log(event)
-            // this.title = event.target.value
+        createPost(post){
+            this.posts.push(post)
         }
     }
 }
@@ -79,32 +52,6 @@ scoped флаг -->
 
 .app {
     padding: 20px;
-}
-
-.post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
-
-form {
-    display: flex;
-    flex-direction: column;
-}
-
-.input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
-
-.btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    border: 1px solid teal;
 }
 
 </style>
--- Js and Css/vue/vueproject/src/components/PostForm.vue
+++ Js and Css/vue/vueproject/src/components/PostForm.vue
@@ -1,15 +1,56 @@
 <template>
-    <div>
-        
-    </div>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+
+        <my-input
+            v-model="post.title"
+            type="text" 
+            placeholder="Название"
+        />
+        <my-input 
+            v-model="post.body"
+            type="text" 
+            placeholder="Описание"
+        />
+        <my-button 
+            style="align-self:flex-end; margin-top: 15px;"
+            @click="createPost"
+        >
+        Создать пост
+        </my-button>
+    </form>
 </template>
 
 <script>
-export default {
+// import MyButton from './UI/MyButton.vue'
+// import MyButton from '@/components/UI/MyButton.vue'
 
+export default {
+    // components: {MyButton},
+    data(){
+        return {
+            post: {
+                title: '',
+                body: ''
+            }
+        }
+    },
+    methods: {
+        createPost(){
+            this.post.id = Date.now()
+            this.$emit('create', this.post)
+            this.post = {
+                title: '',
+                body: ''
+            }
+        },
+    }
 }
 </script>
 
 <style scoped>
-
+form {
+    display: flex;
+    flex-direction: column;
+}
 </style>
