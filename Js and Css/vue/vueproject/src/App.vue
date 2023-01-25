aedc0071ee686d6b9d3da44ab0da11121533764f
lastfighter94 <yurykishko@yandex.ru>
2023-01-25 15:44:23 +0300

    vue_3 continue learning, v-model двухстороннее связывание + нюансы

--- Js and Css/vue/vueproject/src/App.vue
+++ Js and Css/vue/vueproject/src/App.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="app">
-        <post-form
-            @create="createPost"
-        />
+        <h1>Страница с постами</h1>
+        <my-button
+            @click="showDialog"
+        >
+            Создать пользователя
+        </my-button>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form
+                @create="createPost"
+            />
+        </my-dialog>
         <post-list
             :posts="posts"
+            @remove="removePost"
         />
         <!-- <post-list v-bind:posts="posts"/> -->
     </div>
@@ -28,12 +37,20 @@ export default {
                 {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
                 {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
                 {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-            ]
+            ],
+            dialogVisible: false
         }
     },
     methods: {
         createPost(post){
             this.posts.push(post)
+            this.dialogVisible = false
+        },
+        removePost(post){
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        },
+        showDialog(){
+            this.dialogVisible = true
         }
     }
 }
