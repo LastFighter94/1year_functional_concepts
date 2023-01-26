da3e976112a201d7be07e87ec416438505e328a9
lastfighter94 <yurykishko@yandex.ru>
2023-01-26 15:56:57 +0300

    vue_3 continue learning - модификаторы v-model, axios, hooks, индикатор загрузки

--- Js and Css/vue/vueproject/src/App.vue
+++ Js and Css/vue/vueproject/src/App.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="app">
         <h1>Страница с постами</h1>
-        <my-button
-            @click="showDialog"
-        >
-            Создать пользователя
-        </my-button>
+        <div class="app__btns">
+            <my-button
+                @click="showDialog"
+            >
+                Создать пользователя
+            </my-button>
+
+            <my-select
+                v-model="selectedSort"
+                :options="sortOptions"
+            />
+        </div>
+        <!-- <input type="text" v-model.trim="modificatorValue"> -->
+        <!-- модификаторы есть еще - number,  -->
         <my-dialog v-model:show="dialogVisible">
             <post-form
                 @create="createPost"
@@ -14,7 +23,9 @@
         <post-list
             :posts="posts"
             @remove="removePost"
+            v-if="!isPostsLoading"
         />
+        <div v-else>Идет загрузка...</div>
         <!-- <post-list v-bind:posts="posts"/> -->
     </div>
 </template>
@@ -25,6 +36,8 @@ import PostList from '@/components/PostList.vue';
 // import PostForm from './components/PostForm.vue';
 // import PostList from './components/PostList.vue';
 
+import axios from 'axios'
+
 export default {
     components: {
         PostForm,
@@ -32,13 +45,15 @@ export default {
     },
     data(){
         return {
-            posts: [
-                {id: 1, title: 'JavaScript', body: 'Описание поста'},
-                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
-                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
-                {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
-            ],
-            dialogVisible: false
+            posts: [],
+            dialogVisible: false,
+            modificatorValue: '',
+            isPostsLoading: false,
+            selectedSort: '',
+            sortOptions: [
+                {value: 'title', name: 'По названию'},
+                {value: 'body', name: 'По содержанию'},
+            ]
         }
     },
     methods: {
@@ -51,7 +66,25 @@ export default {
         },
         showDialog(){
             this.dialogVisible = true
+        },
+        async fetchPosts(){
+            try {
+                this.isPostsLoading = true
+                setTimeout(async () => {
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                    this.posts = response.data
+                    this.isPostsLoading = false
+                }, 1000)
+
+             } catch (e) {
+                alert('ошибка')
+            } finally {
+                // this.isPostsLoading = false
+            }
         }
+    },
+    mounted(){
+            this.fetchPosts()
     }
 }
 </script>
@@ -69,6 +102,12 @@ scoped флаг -->
 
 .app {
     padding: 20px;
+}
+
+.app__btns {
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
 }
 
 </style>
