bd45c854f654d793c26053c42914e595b58d0080
Юрий Кишко <urijkisko@Urijs-MacBook-Air.local>
2023-02-21 11:03:26 +0800

    fix react router dom moment

--- Js and Css/react/react-fund-course/src/components/AppRouter.jsx
+++ Js and Css/react/react-fund-course/src/components/AppRouter.jsx
@@ -11,24 +11,24 @@ const AppRouter = () => {
     const isAuth = false;
 
     return (
-        // <div>
-        // {isAuth
-        //     ?
-        //     <Routes>
-        //         <Route path='/about' element={<About/>}/>
-        //         <Route path="/error" element={<Error />} />
-        //         <Route exact path='/posts' element={<Posts/>}/>
-        //         <Route exact path="/posts/:id" element={<PostIdPage />} />
-        //         <Route path="/*" element={<Posts/>} />
-        //     </Routes>
-        //     :
-        //     <Routes>
-        //         <Route path='/about' element={<About/>}/>
-        //         <Route path="/error" element={<Error />} />
-        //         <Route path="/*" element={<Login/>} />
-        //     </Routes>
-        // }
-        // </div>
+        <div>
+        {isAuth
+            ?
+            <Routes>
+                <Route path='/about' element={<About/>}/>
+                <Route path="/error" element={<Error />} />
+                <Route exact path='/posts' element={<Posts/>}/>
+                <Route exact path="/posts/:id" element={<PostIdPage />} />
+                <Route path="/*" element={<Posts/>} />
+            </Routes>
+            :
+            <Routes>
+                <Route path='/about' element={<About/>}/>
+                <Route path="/error" element={<Error />} />
+                <Route path="/*" element={<Login/>} />
+            </Routes>
+        }
+        </div>
 
         // <Routes>
         //     <Route path='/about' element={<About/>}/>
@@ -39,25 +39,25 @@ const AppRouter = () => {
         // </Routes>
 
         // не получилось сделать так, чтобы заработало
-        <BrowserRouter>
-            {privateRoutes.map(route =>
-                <Route
-                    key={route.path}
-                    element={route.element}
-                    path={route.path}
-                    exact={route.exact}
-                />
-            )}
-            {publicRoutes.map(route =>
-                <Route
-                    key={route.path}
-                    element={route.component}
-                    path={route.path}
-                    exact={route.exact}
-                />
-            )}
-            <Route path="/*" element={<Navigate to="/error" replace />} />
-        </BrowserRouter>
+        // <BrowserRouter>
+        //     {privateRoutes.map(route =>
+        //         <Route
+        //             key={route.path}
+        //             element={route.element}
+        //             path={route.path}
+        //             exact={route.exact}
+        //         />
+        //     )}
+        //     {publicRoutes.map(route =>
+        //         <Route
+        //             key={route.path}
+        //             element={route.component}
+        //             path={route.path}
+        //             exact={route.exact}
+        //         />
+        //     )}
+        //     <Route path="/*" element={<Navigate to="/error" replace />} />
+        // </BrowserRouter>
     );
 };
 
